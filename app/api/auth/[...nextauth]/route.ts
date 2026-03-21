7ba750f5580e31cd08b0ec86f0af82e7930272cf
Anshit-Gupta <anshitgupta777@gmail.com>
2026-03-21 22:18:57 +0530

    deploy error fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,5 @@
 import NextAuth from "next-auth";
-import type { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import  CredentialsProvider  from "next-auth/providers/credentials";
-import {compare} from 'bcryptjs'
-import {PrismaAdapter} from "@auth/prisma-adapter";
-import {prisma} from "@/lib/prisma"
-
-
-//[...nextauth] is a catch-all dynamic route in Next.js. 
-
-export const authOptions: NextAuthOptions = {
-    adapter:PrismaAdapter(prisma),
-    providers : [
-        GoogleProvider({
-                 clientId: process.env.GOOGLE_CLIENT_ID!,
-                 clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        }),
-
-        CredentialsProvider({
-            name:"Credentials",
-            credentials : {   //metadata for the Credentials provider
-                email : {label:"Email" , type:"email"},
-                password : {label:"Password" , type:"password"}
-            },
-            async authorize(credentials){
-                if(! credentials?.email || ! credentials?.password ){
-                       return null;
-                } 
-               const user =  await prisma.user.findUnique(
-                    {
-                        where : {email : credentials.email}
-                    }
-                )
-                if(!user) return null;
-                if(! user.password) return null; // as password is optional in user schema 
-                 const isValidPassword = await compare(credentials.password , user.password);
-
-                if(! isValidPassword) return null;
-                
-                return {id:user.id , email:user.email , name: user.name}
-                
-                
-
-            } 
-        })
-    ]
-}
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 export {handler as GET , handler as POST}
--- app/api/shorturl/route.ts
+++ app/api/shorturl/route.ts
@@ -3,7 +3,7 @@ import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
 import { urlSchema } from "@/lib/schema/url";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 
 export async function POST(request: Request) {
